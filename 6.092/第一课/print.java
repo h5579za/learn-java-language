aa46617d46422f1e8b17235e7b11f19702f27c48
h5579za <h5579za@outlook.com>
2024-10-03 18:58:43 +0800

    第一课代码

---
+++ 6.092/第一课/print.java
@@ -0,0 +1,10 @@
+//package 6.092;
+
+public class print 
+{
+   public static void main(String[] arguments) 
+   {
+        System.out.println("Hello world."); // 打印一次
+        System.out.println("Line number 2"); // 再次打印 
+   }
+}
---
+++ 6.092/第一课/赋值.java
@@ -0,0 +1,10 @@
+//package 6.092.第一课;
+
+public class 赋值 {
+   public static void main(String[] arguments) {
+        String foo = "IAP 6.092";
+        System.out.println(foo);
+        foo = "Something else";
+        System.out.println(foo);
+    } 
+}
